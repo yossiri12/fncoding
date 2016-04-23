43ce8267e3e2d618a6f8e9ef7c67f111ec927fcd
PTE ENGINEERING <PTE ENGINEERING@PTEENGINEERING>
2016-04-24 01:20:48 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,6 +1,25 @@
 <?php
-
+use yii\helpers\Html;
 /* @var $this yii\web\View */
 
 $this->title = 'FnCoDing';
 ?>
+
+
+<?php
+$route1=  Yii::$app->urlManager->createUrl("test/test1");
+?>
+<a href="<?=$route1 ?>">ไปที่ test1</a>
+<br>
+
+<?php
+$route2=  Yii::$app->urlManager->createAbsoluteUrl(["test/test2", 'name'=>"yossiri"]);
+?>
+<a href="<?=$route2 ?>">ไปที่ test2</a>
+<br>
+
+<?=
+Html::a('ลิ้งไปที่ 3',['test/test1']);        
+?>
+
+<br>
